f76031fb9608d8dcd77a4bbb49435a6bdcd2fee6
DenRisk <35171305+DenRisk@users.noreply.github.com>
2022-04-02 17:01:58 +0200

    master: setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderModule } from './components/header/header.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ComponentsModule} from "./components/components.module";
+import {HeaderModule} from "./components/header/header.module";
 
 @NgModule({
   declarations: [
@@ -12,8 +13,11 @@ import { HeaderModule } from './components/header/header.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ComponentsModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
