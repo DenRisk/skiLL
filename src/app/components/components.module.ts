4dd217b5fed719670011d5b2f664dbf058c86261
DenRisk <DenRisk@users.noreply.github.com>
2022-04-18 10:48:32 +0200

    feat(master): add config and header, variables , stylesheets

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,16 @@
 import {NgModule} from "@angular/core";
 import {HeaderModule} from "./header/header.module";
+import { SplashStageComponent } from './splashstage/splash-stage.component';
 
 @NgModule({
-  imports: [
-    HeaderModule
-  ]
+    imports: [
+        HeaderModule
+    ],
+    exports: [
+        SplashStageComponent
+    ],
+    declarations: [
+        SplashStageComponent
+    ]
 })
 export class ComponentsModule {}
---
+++ src/app/components/splashstage/splash-stage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'skl-splash-stage',
+  templateUrl: './splash-stage.component.html',
+  styleUrls: ['./splash-stage.component.scss']
+})
+export class SplashStageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
